0d9ee7220d4feb81e0bdb329f911485bb0e8ee2a
dave-schroda <130723314+dave-schroda@users.noreply.github.com>
2023-04-14 01:16:54 -0400

    Update custom-product-options.php

--- custom-product-options.php
+++ custom-product-options.php
@@ -6,66 +6,8 @@
  * Author: Your Name
  * Author URI: https://yourwebsite.com
  */
+require_once plugin_dir_path( __FILE__ ) . 'custom-product-options-settings.php';
 
-// Define the markup percentage as a global variable
-global $custom_markup_percentage;
-$custom_markup_percentage = get_option('markup_percentage');
-
-// Add the settings menu
-function custom_product_options_add_settings_link($links) {
-  $settings_link = '<a href="admin.php?page=custom-product-options-settings">' . __('Settings') . '</a>';
-  array_push($links, $settings_link);
-  return $links;
-}
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'custom_product_options_add_settings_link');
-
-function custom_product_options_settings_page() {
-  ?>
-  <div class="wrap">
-    <h1><?php esc_html_e( 'Custom Product Options Settings', 'custom-product-options' ); ?></h1>
-    <form method="post" action="options.php">
-      <?php settings_fields( 'custom_product_options_settings_group' ); ?>
-      <?php do_settings_sections( 'custom_product_options_settings_group' ); ?>
-      <table class="form-table">
-        <tr>
-          <th scope="row">
-            <label for="markup_percentage"><?php esc_html_e( 'Markup Percentage', 'custom-product-options' ); ?></label>
-          </th>
-          <td>
-            <input type="number" name="markup_percentage" id="markup_percentage" value="<?php echo esc_attr( get_option( 'markup_percentage' ) ); ?>" class="regular-text" min="0" max="100" step="0.01">
-            <p class="description"><?php esc_html_e( 'Enter the markup percentage to add to the price of each product.', 'custom-product-options' ); ?></p>
-          </td>
-        </tr>
-      </table>
-      <?php submit_button(); ?>
-    </form>
-  </div>
-  <?php
-}
-
-function custom_product_options_get_option($option_name) {
-  global $custom_markup_percentage;
-
-  $$option_value = get_option($option_name, 0);
-
-  if (!$option_value) {
-    switch ($option_name) {
-      case 'markup_percentage':
-        if ($custom_markup_percentage) {
-          $option_value = $custom_markup_percentage;
-        } else {
-          $option_value = 1;
-        }
-        break;
-      default:
-        $option_value = '';
-        break;
-    }
-    add_option($option_name, $option_value);
-  }
-
-  return $option_value;
-}
 
 // Enqueue the JavaScript files
 function custom_product_options_enqueue_scripts() {
