d8da2e4ab9e7a2e4f338ec643a7858846008d30c
dave-schroda <130723314+dave-schroda@users.noreply.github.com>
2023-04-14 14:34:31 -0400

    Update custom-product-options.php

--- custom-product-options.php
+++ custom-product-options.php
@@ -39,3 +39,73 @@ function custom_product_options_add_menu_item() {
   );
 }
 add_action('admin_menu', 'custom_product_options_add_menu_item');
+
+/**
+ * Add custom product options to cart item data
+ */
+function custom_product_options_add_to_cart( $cart_item_data, $product_id, $variation_id ) {
+    $custom_data = array();
+
+    // Get selected options
+    if ( isset( $_POST['woodSelect'] ) ) {
+        $custom_data['Wood'] = sanitize_text_field( $_POST['woodSelect'] );
+    }
+
+    if ( isset( $_POST['sizeSelect'] ) ) {
+        $custom_data['Size'] = sanitize_text_field( $_POST['sizeSelect'] );
+    }
+
+    if ( ! empty( $custom_data ) ) {
+        $cart_item_data['custom_data'] = $custom_data;
+    }
+
+    return $cart_item_data;
+}
+add_filter( 'woocommerce_add_cart_item_data', 'custom_product_options_add_to_cart', 10, 3 );
+
+/**
+ * Display selected options on cart and checkout pages
+ */
+function custom_product_options_display_cart( $item_data, $cart_item ) {
+    if ( isset( $cart_item['custom_data'] ) ) {
+        $custom_data = $cart_item['custom_data'];
+
+        // Display selected options
+        if ( isset( $custom_data['Wood'] ) ) {
+            $item_data[] = array(
+                'key'   => 'Wood',
+                'value' => $custom_data['Wood']
+            );
+        }
+
+        if ( isset( $custom_data['Size'] ) ) {
+            $item_data[] = array(
+                'key'   => 'Size',
+                'value' => $custom_data['Size']
+            );
+        }
+    }
+
+    return $item_data;
+}
+add_filter( 'woocommerce_get_item_data', 'custom_product_options_display_cart', 10, 2 );
+
+/**
+ * Save selected options to order
+ */
+function custom_product_options_order_meta_handler( $item_id, $values, $cart_item_key ) {
+    if ( isset( $values['custom_data'] ) ) {
+        $custom_data = $values['custom_data'];
+
+        // Save selected options to order
+        if ( isset( $custom_data['Wood'] ) ) {
+            wc_add_order_item_meta( $item_id, 'Wood', $custom_data['Wood'] );
+        }
+
+        if ( isset( $custom_data['Size'] ) ) {
+            wc_add_order_item_meta( $item_id, 'Size', $custom_data['Size'] );
+        }
+    }
+}
+add_action( 'woocommerce_add_order_item_meta', 'custom_product_options_order_meta_handler', 10, 3 );
+
